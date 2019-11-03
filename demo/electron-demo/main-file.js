912f4f40b7a5f43250f6f548017a04578b3c3312
Ender <xenderpugx@gmail.com>
2019-11-03 19:29:32 +0000

    🍦 Yogurt pot update

---
+++ demo/electron-demo/main-file.js
@@ -0,0 +1,2 @@
+console.log("The next line will fail because console.log is spelt wrong.");
+comsoole.log("Wow! You fixed it.")
--- yogurt.js
+++ yogurt.js
@@ -1,4 +1,4 @@
-const { yogurt, task } = require('./src/bundle.js');
+const { yogurt } = require('./src/bundle.js');
 
 const bundle = new yogurt({
   name: 'main',
@@ -6,14 +6,7 @@ const bundle = new yogurt({
   output: '/out',
   log: {
     showBundledFiles: true,
-  },
-  tasks: [{
-    name: 'done',
-    command: 'node out/main-file.js'
-  }, {
-    name: 'are-we-there-yet',
-    command: 'node out/multi_layer_folders.js'
-  }]
+  }
 });
 
 bundle.start();
